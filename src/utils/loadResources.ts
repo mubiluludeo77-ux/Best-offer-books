ccf0161ce8ef4574dbe68a4e084ae09e44079533
Chadrac <dabirechadrac6@gmail.com>
2026-07-22 05:32:11 -0400

    chore(i18n): ajout des fichiers de traduction

---
+++ src/utils/loadResources.ts
@@ -0,0 +1,42 @@
+import commonFr from '@/locales/fr/common.json';
+import headerFr from '@/locales/fr/header.json';
+import footerFr from '@/locales/fr/footer.json';
+import homeFr from '@/locales/fr/home.json';
+import contactFr from '@/locales/fr/contact.json';
+import booksFr from '@/locales/fr/books.json';
+import categoriesFr from '@/locales/fr/categories.json';
+import cartFr from '@/locales/fr/cart.json';
+
+import commonEn from '@/locales/en/common.json';
+import headerEn from '@/locales/en/header.json';
+import footerEn from '@/locales/en/footer.json';
+import homeEn from '@/locales/en/home.json';
+import contactEn from '@/locales/en/contact.json';
+import booksEn from '@/locales/en/books.json';
+import categoriesEn from '@/locales/en/categories.json';
+import cartEn from '@/locales/en/cart.json';
+
+const resources = {
+  fr: {
+    common: commonFr,
+    header: headerFr,
+    footer: footerFr,
+    home: homeFr,
+    contact: contactFr,
+    books: booksFr,
+    categories: categoriesFr,
+    cart: cartFr,
+  },
+  en: {
+    common: commonEn,
+    header: headerEn,
+    footer: footerEn,
+    home: homeEn,
+    contact: contactEn,
+    books: booksEn,
+    categories: categoriesEn,
+    cart: cartEn,
+  },
+};
+
+export default resources;
